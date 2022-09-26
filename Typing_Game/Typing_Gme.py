ee7703e467560137a50f3934fd8bdce5c751d60e
TimTheRabbit <87847364+TimTheRabbit@users.noreply.github.com>
2022-09-26 20:29:54 +0800

    Add files via upload

--- Typing_Game/Typing_Gme.py
+++ Typing_Game/Typing_Gme.py
@@ -1,9 +1,11 @@
-from pycat.core import Window,Sprite,Label,KeyCode,Scheduler
+from pycat.core import Window,Sprite,Label,KeyCode,Scheduler,Point,Color
 from pycat.base.event import KeyEvent
 import random
 window = Window()
 
-words = ["gnoy","ekim","imt","david","honjnos"]
+words = ["gnoy","ekim","imt","david","honjnos","pneumonoultramicroscopicsilicovolcanoconiosis"]
+enemy_start_x = [0,1280]
+enemy_start_y = [0,640]
 
 class Typable_Label(Label):
     def on_create(self):
@@ -19,8 +21,10 @@ class Typable_Label(Label):
             self.text = self.text [1:]
 class Enemy(Sprite):
     def on_create(self):
+        self.position = Point(random.choice(enemy_start_x),random.choice(enemy_start_y))
         self.scale = 25
         self.label = window.create_label(Typable_Label)
+        self.color = Color.RED
         
     def on_update(self, dt):
         self.label.position = self.position
@@ -30,8 +34,11 @@ class Enemy(Sprite):
             self.label.delete()
             self.delete()
         if self.is_touching_sprite(typer):
-            self.delete()
             self.label.delete()
+            typer.life -= 1
+            life.text = "life: "+str(typer.life)
+            self.delete()
+            
             
 class Typer(Sprite):
     def on_create(self):
@@ -39,7 +46,8 @@ class Typer(Sprite):
         self.scale_y = 50
         self.x = 640
         self.y = 320
-        self.color = 200,200,200
+        self.color = Color.CYAN
+        self.life = 10
             
 
 def create_enemy():
@@ -51,4 +59,5 @@ Scheduler.update(create_enemy,3)
 #enemy = window.create_sprite(Enemy)
 typer = window.create_sprite(Typer)
 #window.create_label(Typable_Label)
+life = window.create_label(text = "life: "+str(typer.life),x = typer.x,y = typer.y)
 window.run()
