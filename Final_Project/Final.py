ce684899afc40bc82e5898f2ac7403f0ed9c078c
TimTheRabbit <87847364+TimTheRabbit@users.noreply.github.com>
2023-01-07 17:40:51 +0800

    Add files via upload

--- Final_Project/Final.py
+++ Final_Project/Final.py
@@ -3,7 +3,6 @@ from pycat.extensions.ldtk import LdtkLayeredLevel
 import pyglet
 
 window = Window(width=1024,height=512)
-
 level = LdtkLayeredLevel.from_file(
     ldtk_file_path="level.ldtk",
     level_id="Level_0",
@@ -12,7 +11,15 @@ level = LdtkLayeredLevel.from_file(
         "Tiles":-1
     }
 )
-level.render(window,scale=2,debug_entities=True)
+level.render(window,scale=2,debug_entities=False)
+
+end_cond = False
+
+class Win_Label(Label):
+    def on_create(self):
+        self.is_visible = False
+        self.font_size = 50
+        self.text = ""
 
 class WrapSprite(Sprite):
     def wrap(self):
@@ -76,27 +83,61 @@ class Player(WrapSprite):
         if self.get_SP >= 1:
             if "p1" in self.tags:
                 print("p1 super")
-                p2.keep_hold *= 0.5
-                p2h.text = "Keeping Time: "+str(round(p2.keep_hold,1))
-                self.get_SP = 0
+                if p2.keep_hold > 0:
+                    p2.keep_hold *= 0.5
+                    p2h.text = "Keeping Time: "+str(round(p2.keep_hold,1))
+                    self.get_SP = 0
+                else:
+                    p2.bullet *= 0.5
+                    p2_bullet.text = "Left Bullet: "+str(round(p2.bullet,1))
+                    self.get_SP = 0
             elif "p2" in self.tags:
                 print("p2 super")
-                p1.keep_hold *= 0.5
-                p1h.text = "Keeping Time: "+str(round(p1.keep_hold,1))
-                self.get_SP = 0
+                if p1.keep_hold > 0:
+                    p1.keep_hold *= 0.5
+                    p1h.text = "Keeping Time: "+str(round(p1.keep_hold,1))
+                    self.get_SP = 0
+                else:
+                    p1.bullet *= 0.5
+                    p1_bullet.text = "Left Bullet: "+str(round(p1.bullet,1))
+                    self.get_SP = 0
     def change_bar_costume(self,target_sprite):
         if self.get_SP == 0:
             target_sprite.scale_x = 0
-        if self.get_SP <= 0.2 and self.get_SP > 0:
+        if self.get_SP <= 0.05 and self.get_SP > 0.02:
+            target_sprite.scale_x = 0.03
+        elif self.get_SP <= 0.075 and self.get_SP > 0.05:
+            target_sprite.scale_x = 0.07
+        elif self.get_SP <= 0.1 and self.get_SP > 0.075:
+            target_sprite.scale_x = 0.1
+        elif self.get_SP <= 0.15 and self.get_SP > 0.1:
+            target_sprite.scale_x = 0.15
+        elif self.get_SP <= 0.2 and self.get_SP > 0.15:
             target_sprite.scale_x = 0.2
-        elif self.get_SP <= 0.4 and self.get_SP > 0.2:
+        elif self.get_SP <= 0.25 and self.get_SP > 0.2:
+            target_sprite.scale_x = 0.25
+        elif self.get_SP <= 0.33 and self.get_SP > 0.25:
+            target_sprite.scale_x = 0.3    
+        elif self.get_SP <= 0.4 and self.get_SP > 0.33:
             target_sprite.scale_x = 0.4
-        elif self.get_SP <= 0.6 and self.get_SP > 0.4:
+        elif self.get_SP <= 0.45 and self.get_SP > 0.4:
+            target_sprite.scale_x = 0.45
+        elif self.get_SP <= 0.5 and self.get_SP > 0.45:
+            target_sprite.scale_x = 0.5
+        elif self.get_SP <= 0.55 and self.get_SP > 0.5:
+            target_sprite.scale_x = 0.55
+        elif self.get_SP <= 0.6 and self.get_SP > 0.55:
             target_sprite.scale_x =0.6
         elif self.get_SP <= 0.75 and self.get_SP > 0.6:
             target_sprite.scale_x = 0.75
-        elif self.get_SP <= 0.9 and self.get_SP > 0.75:
+        elif self.get_SP <= 0.8 and self.get_SP > 0.75:
+            target_sprite.scale_x = 0.8
+        elif self.get_SP <= 0.85 and self.get_SP > 0.8:
+            target_sprite.scale_x = 0.85
+        elif self.get_SP <= 0.9 and self.get_SP > 0.85:
             target_sprite.scale_x = 0.9
+        elif self.get_SP <= 0.95 and self.get_SP > 0.9:
+            target_sprite.scale_x = 0.97
     def on_joyaxis_motion(self,joystick,axis,value):
         if axis == "x":
             self.joy_x = value
@@ -106,6 +147,9 @@ class Player(WrapSprite):
         print("button: ",button)
         self.button = button
     def on_update(self, dt):
+        global end_cond
+        if end_cond== True:
+            self.delete()
         self.wrap()
         if window.is_key_down(self.super) or self.button == 0:
             self.super_power()
@@ -128,7 +172,7 @@ class Player(WrapSprite):
         if window.is_key_pressed(self.up_key) or self.joy_y < -0.2:
             self.y+=10
             if self.get_SP<1:
-                self.get_SP += 0.005
+                self.get_SP += 0.0008
             if self.is_touching_any_sprite_with_tag("ldtk_wall"):
                 self.y -= 10
                 self.drop_power()
@@ -136,7 +180,7 @@ class Player(WrapSprite):
         elif window.is_key_pressed(self.down_key) or self.joy_y > 0.2:
             self.y -= 10
             if self.get_SP<1:
-                self.get_SP += 0.005
+                self.get_SP += 0.0008
             if self.is_touching_any_sprite_with_tag("ldtk_wall"):
                 self.y += 10
                 self.drop_power()
@@ -144,7 +188,7 @@ class Player(WrapSprite):
         elif window.is_key_pressed(self.left_key) or self.joy_x < -0.2:
             self.x -= 10
             if self.get_SP<1:
-                self.get_SP += 0.001
+                self.get_SP += 0.0008
             self.rotation = 180
             if self.is_touching_any_sprite_with_tag("ldtk_wall"):
                 self.x += 10
@@ -153,7 +197,7 @@ class Player(WrapSprite):
         elif window.is_key_pressed(self.right_key) or self.joy_x > 0.2:
             self.x += 10
             if self.get_SP<1:
-                self.get_SP += 0.001
+                self.get_SP += 0.0008
             self.rotation = 0
             if self.is_touching_any_sprite_with_tag("ldtk_wall"):
                 self.x -= 10
@@ -162,8 +206,22 @@ class Player(WrapSprite):
         if window.is_key_down(self.shoot_key) or self.button == 1:
             self.shoot()
             self.button = None
-        if self.keep_hold >= 60:
-            window.close()       
+        if self.keep_hold >= 10:
+            print("one win")
+            win_l = window.create_label(Win_Label)
+            win_l.position = self.position
+            if "p1" in self.tags:
+                win_l.is_visible =True
+                win_l.color = Color.BLUE
+                win_l.text = "P1 win the great battle!!!" 
+
+                end_cond = True  
+            else:
+                win_l.is_visible =True
+                win_l.color = Color.RED
+                win_l.text = "P2 win the great battle!!!"  
+
+                end_cond = True
 class Power(Sprite):
     def on_create(self):
         self.image = "target.png"
@@ -172,6 +230,9 @@ class Power(Sprite):
         self.scale = 0.035
         self.goto_random_position_in_region(32+128,32+128,1024-128,512-128)
     def on_update(self, dt):
+        global end_cond
+        if end_cond== True:
+            self.delete()
         if self.is_touching_any_sprite_with_tag("p1") and (window.is_key_down(p1.pick_key) or p1.button == 2):
             p1.holding=True
             p1.button = None
@@ -195,6 +256,9 @@ class Power(Sprite):
             self.goto_random_position_in_region(32+128,32+128,1024-128,512-128)
 class Bullet_Collection(Sprite):
     def on_create(self):
+        global end_cond
+        if end_cond== True:
+            self.delete()
         self.image = "BULLET.png"
         self.scale = 0.05
         self.goto_random_position()
@@ -246,6 +310,8 @@ class Bullet_Shooting(Sprite):
 def Spawn_Bullet():
     window.create_sprite(Bullet_Collection)
 Scheduler.update(Spawn_Bullet,5)
+if end_cond== True:
+    Scheduler.cancel_update(Spawn_Bullet)
 
 #joysticks set up
 joysticks = pyglet.input.get_joysticks()
